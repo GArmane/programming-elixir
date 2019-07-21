84881b63580809e97fc95897886594d63493e1cd
Giovanni Armane <giovanniarmane@gmail.com>
2019-07-21 20:55:00 -0300

    Chapter 13 project: parses data to table

--- Chapter 13/issues/mix.exs
+++ Chapter 13/issues/mix.exs
@@ -21,7 +21,8 @@ defmodule Issues.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:httpoison, "~> 0.4"}
+      { :httpoison, "~> 0.4" },
+      { :jsx,       "~> 2.0" }
     ]
   end
 end
