74963032983491ad4cbc74bef04c2d2db781cdd6
Giovanni Armane <giovanniarmane@gmail.com>
2019-08-14 22:25:33 -0300

    Add ExDoc and generate docs

--- Chapter 13/issues/mix.exs
+++ Chapter 13/issues/mix.exs
@@ -5,6 +5,8 @@ defmodule Issues.MixProject do
     [
       app: :issues,
       version: "0.1.0",
+      name: "Issues",
+      source_url: "https://github.com/GArmane/programming-elixir/tree/master/Chapter%2013/issues",
       elixir: "~> 1.8",
       escript: escript_config(),
       start_permanent: Mix.env() == :prod,
@@ -23,7 +25,8 @@ defmodule Issues.MixProject do
   defp deps do
     [
       { :httpoison, "~> 0.4" },
-      { :jsx,       "~> 2.0" }
+      { :jsx,       "~> 2.0" },
+      { :ex_doc,    github: "elixir-lang/ex_doc" },
     ]
   end
 
