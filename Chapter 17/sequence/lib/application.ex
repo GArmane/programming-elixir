bffc5012ec1411714b5b80dc8aeb90abc90d7306
Giovanni Armane <giovanniarmane@gmail.com>
2019-09-19 21:00:08 -0300

    Add stash and subsupervisors to Sequence

--- Chapter 17/sequence/lib/application.ex
+++ Chapter 17/sequence/lib/application.ex
@@ -4,11 +4,6 @@ defmodule Sequence.Application do
   use Application
 
   def start(_type, _args) do
-    children = [
-      { Sequence.Server, 123 }
-    ]
-
-    opts = [strategy: :one_for_one, name: Sequence.Supervisor]
-    Supervisor.start_link(children, opts)
+    {:ok, _pid} = Sequence.Supervisor.start_link(0)
   end
 end
---
+++ Chapter 17/sequence/lib/sequence/supervisor.ex
@@ -0,0 +1,21 @@
+defmodule Sequence.Supervisor do
+  use Supervisor
+
+  def start_link(initial_number) do
+    result = {:ok, sup} = Supervisor.start_link(__MODULE__, [initial_number])
+    start_workers(sup, initial_number)
+    result
+  end
+
+  def start_workers(sup, initial_number) do
+    {:ok, stash} = Supervisor
+      .start_child(sup, worker(Sequence.Stash, [initial_number]))
+
+    {:ok, _pid} = Supervisor
+      .start_child(sup, supervisor(Sequence.SubSupervisor, [stash]))
+  end
+
+  def init(_) do
+    Supervisor.init([], strategy: :one_for_one)
+  end
+end
